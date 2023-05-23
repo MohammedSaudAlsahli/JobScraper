ee8ab12ca105673cbf98a30ed75921720e3f1952
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2023-05-23 07:44:31 +0300

    i don't what i did

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ with open('./jobscraper/README.md', 'r') as f:
 setup(
     name='jobs_scraper',
     version='0.0.1',
-    long_description=long_description, 
+    long_description=long_description,
+    long_description_content_type = 'text/markdown',
     author='Mohammed Alsali',
     url='https://github.com/MohammedSaudAlsahli',
     packages=['jobscraper'],
